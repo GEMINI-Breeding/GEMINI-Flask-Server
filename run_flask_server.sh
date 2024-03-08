4ac079a5c41d8adda69e4542bc014f657686741f
eranario <ewranario@ucdavis.edu>
2024-03-07 23:31:05 -0800

    deployment changes

--- run_flask_server.sh
+++ run_flask_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pushd /home/gemini/scratch/GEMINI-Flask-Server # Change to the directory of GEMINI-Flask-Server
+pushd /home/gemini/GEMINI-Flask-Server # Change to the directory of GEMINI-Flask-Server
 # bash ./install_flask_server.sh
 
 # Activate conda env
@@ -7,14 +7,14 @@ source /home/gemini/miniconda3/bin/activate .conda/
 
 # Use default arguments when they are not provided (data_root_dir, port)
 if [ -z "$1" ]; then
-    data_root_dir="/home/gemini/data/GEMINI-App-Data"
+    data_root_dir="/mnt/d/GEMINI-App-Data"
 else
     data_root_dir=$1
 fi
 echo $data_root_dir
 
 if [ -z "$2" ]; then
-    port=5003
+    port=5050
 else
     port=$2
 fi
