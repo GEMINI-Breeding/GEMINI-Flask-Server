1e8ef7fc24c83c926a9bc8477f745e926b4e70aa
Heesup Yun <hspyun@ucdavis.edu>
2024-05-08 22:53:41 -0700

    Now it works on Mac

--- run_flask_server.sh
+++ run_flask_server.sh
@@ -6,7 +6,12 @@ pushd $(pwd)/../GEMINI-Flask-Server
 # bash ./install_flask_server.sh
 
 # Read conda path from your ~/.bashrc and parse the conda path before :$PATH
-conda_path=$(grep -oP 'export PATH="\K[^:]+/bin' ~/.bashrc)
+if [[ "$(uname)" == "Darwin" ]]; then
+    conda_path=$(pcregrep -o '__conda_setup="\$\(\K[^:]+/bin' ~/.zshrc | awk -F':' '{print $1}' | tr -d "'")
+else
+    conda_path=$(grep -oP 'export PATH="\K[^:]+/bin' ~/.bashrc)
+fi
+
 echo "Conda Path: $conda_path"
 # Activate conda environment
 source $conda_path/activate .conda/
@@ -26,7 +31,13 @@ fi
 echo "data_root_dir: $data_root_dir"
 
 # Read the port from gemini-app/src/DataContext.js
-port=$(grep -oP 'const \[flaskUrl, setFlaskUrl\] = useState\("http://127.0.0.1:\K[0-9]+' ../gemini-app/src/DataContext.js)
+if [[ "$(uname)" == "Darwin" ]]; then
+    # Use pcregrep on macOS
+    port=$(pcregrep -o1 'const \[flaskUrl, setFlaskUrl\] = useState\("http://127.0.0.1:\K([0-9]+)' ../gemini-app/src/DataContext.js)
+else
+    # Use grep -P on other systems
+    port=$(grep -oP 'const \[flaskUrl, setFlaskUrl\] = useState\("http://127.0.0.1:\K[0-9]+' ../gemini-app/src/DataContext.js)
+fi
 echo "Flask Port: $port"
 
 python src/app_flask_backend.py --data_root_dir $data_root_dir --port $port
